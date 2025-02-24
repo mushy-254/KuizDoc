d6f43aed5283fa861d13a26ad0429b959f3ee6f7
mushy_254 <janetmumbian@gmail.com>
2025-02-24 13:28:22 +0300

    addd doc upload

--- learning/views.py
+++ learning/views.py
@@ -2,8 +2,19 @@ from django.shortcuts import render
 from .models import UserProfile, Quiz
 
 def dashboard(request):
-    # profile = UserProfile.objects.get(user=request.user)
-    # quizzes = Quiz.objects.filter(category='PHY')[:3]  # Example physics quizzes
-    
-    # return render(request, 'dashboard.html}')
-    return render(request, 'dashboard.html')
+    context = {
+        'active_section': request.GET.get('section', 'dashboard'),
+        'stats': [
+            {'value': '92%', 'label': 'Average Score'},
+            {'value': '15', 'label': 'Days Streak'},
+            {'value': '247', 'label': 'Questions Answered'}
+        ],
+        'progress_labels': ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        'progress_data': [65, 78, 82, 75, 90, 85, 88],
+        'documents': [
+            {'title': 'Quantum Physics Notes', 'last_modified': '2 hours ago'},
+            # ... other documents
+        ],
+        'user': request.user
+    }
+    return render(request, 'dashboard.html', context)
